d48f4cb34e0d47555caffb83e73ad29dd1165f2d
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2024-12-16 12:01:20 -0500

    .

--- infra/main.tf
+++ infra/main.tf
@@ -22,15 +22,8 @@ data "azurerm_client_config" "OperationsSub" {
 
 data "azurerm_client_config" "SharedServicesSub" {
   provider = azurerm.SHAREDSERVICESSub
-}
 
-resource "random_string" "random" {
-  length  = 5
-  special = false
-  upper   = false
-  number  = false
 }
-
 module "entraObjects" {
   source                            = "./core/aad"
   ResourceNamingConvention = var.ResourceNamingConvention
