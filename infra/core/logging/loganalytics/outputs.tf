59a73c82235456994312d409278a7d5a62b552c5
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2024-12-17 08:42:46 -0500

    .

--- infra/core/logging/loganalytics/outputs.tf
+++ infra/core/logging/loganalytics/outputs.tf
@@ -3,7 +3,7 @@ output "applicationInsightsId" {
 }
 
 output "logAnalyticsId" {
-  value = azurerm_log_analytics_workspace.logAnalytics.id
+  value = data.azurerm_log_analytics_workspace.ExistingLAW.id
 }
 
 output "applicationInsightsName" {
@@ -11,7 +11,7 @@ output "applicationInsightsName" {
 }
 
 output "logAnalyticsName" {
-  value = azurerm_log_analytics_workspace.logAnalytics.name
+  value = data.azurerm_log_analytics_workspace.ExistingLAW.name
 }
 
 output "applicationInsightsInstrumentationKey" {
@@ -20,16 +20,4 @@ output "applicationInsightsInstrumentationKey" {
 
 output "applicationInsightsConnectionString" {
   value = azurerm_application_insights.applicationInsights.connection_string
-}
-
-output "privateEndpointId" {
-  value = var.is_secure_mode ? azurerm_private_endpoint.ampls[0].id : null
-}
-
-output "privateEndpointName" {
-  value = var.is_secure_mode ? azurerm_private_endpoint.ampls[0].name : null
-}
-
-output "privateEndpointIpAddress" {
-  value = var.is_secure_mode ? azurerm_private_endpoint.ampls[0].private_service_connection[0].private_ip_address : null
 }
