f1a96666e64ad87c41f69af16b808d65cc8c75d1
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2024-12-17 08:47:44 -0500

    .

--- infra/core/logging/loganalytics/outputs.tf
+++ infra/core/logging/loganalytics/outputs.tf
@@ -2,16 +2,8 @@ output "applicationInsightsId" {
   value = azurerm_application_insights.applicationInsights.id
 }
 
-output "logAnalyticsId" {
-  value = data.azurerm_log_analytics_workspace.ExistingLAW[count.index].id
-}
-
 output "applicationInsightsName" {
   value = azurerm_application_insights.applicationInsights.name
-}
-
-output "logAnalyticsName" {
-  value = data.azurerm_log_analytics_workspace.ExistingLAW[count.index].id
 }
 
 output "applicationInsightsInstrumentationKey" {
