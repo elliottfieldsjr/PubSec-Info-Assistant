74cefc7ef85108a3915b80c6c80bd4a280081d71
Elliott Fields Jr. <34408457+elliottfieldsjr@users.noreply.github.com>
2024-12-17 18:05:36 +0000

    .

--- infra/core/ai/openaiservices/openaiservices.tf
+++ infra/core/ai/openaiservices/openaiservices.tf
@@ -70,7 +70,7 @@ data "azurerm_subnet" "subnet" {
 resource "azurerm_private_endpoint" "openaiPrivateEndpoint" {
   count                         = var.useExistingAOAIService ? 0 : var.is_secure_mode ? 1 : 0
   name                          = "${var.name}-private-endpoint"
-  location                      = var.location
+  location                      = "usgovarizona"
   resource_group_name           = var.resourceGroupName
   subnet_id                     = data.azurerm_subnet.subnet[0].id
   custom_network_interface_name = "infoasstaoainic"
