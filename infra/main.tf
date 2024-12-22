d5f1d1e655135477aa8cbe6cee9126b7635e42ab
elliottfieldsjr <github@mcitpnetworks.com>
2024-12-22 22:01:51 +0000

    .

--- infra/main.tf
+++ infra/main.tf
@@ -26,8 +26,8 @@ locals {
   BingSearchName        = "${var.ResourceNamingConvention}-bing-va"  
   FunctionAppName       = "${var.ResourceNamingConvention}-func-va"
   FunctionASPName       = "${var.ResourceNamingConvention}-func-asp-va"  
-  CertificateName       = "vonnzy-wildcard"
-  CertificateFileName   = "vonnzy.pfx"
+  CertificateName       = "TestCert1"
+  CertificateFileName   = "testcert1.pfx"
   CertificatePassword   = var.CertificatePassword
 }
 
