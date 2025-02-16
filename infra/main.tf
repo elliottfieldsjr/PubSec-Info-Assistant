d0d8817bee1ffd760ac77b82be0f07839fb1e3e1
elliottfieldsjr <github@mcitpnetworks.com>
2025-02-16 18:23:43 +0000

    .

--- infra/main.tf
+++ infra/main.tf
@@ -26,7 +26,7 @@ locals {
   BingSearchName        = "${var.ResourceNamingConvention}-bing-va"  
   FunctionAppName       = "${var.ResourceNamingConvention}-func-va"
   FunctionASPName       = "${var.ResourceNamingConvention}-func-asp-va"  
-  CertificateName       = "BAMIWildcard"
+  CertificateName       = "BAMI1Wildcard"
   CertificateFileName   = "bami1elliottfieldsjr.pfx"
   CertificateFilePath   = "./certificates/${local.CertificateFileName}"
   CertificatePassword   = var.CertificatePassword  
